f592f4583383aabab37728ab6882c80c63debc4c
multitalented box <multitalentedbox@gmail.com>
2025-07-20 18:54:51 +0000

    use this https://www.sadc.int/sites/default/files/2022-02/Peter-Mutharik

--- next.config.ts
+++ next.config.ts
@@ -25,6 +25,10 @@ const nextConfig: NextConfig = {
       {
         protocol: 'https',
         hostname: 'www.nyasatimes.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.sadc.int',
       }
     ],
   },
--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -5,7 +5,7 @@ import { handleBetPlacement } from "@/actions/bet"
 
 const candidates = [
   { id: 1, name: "Lazarus Chakwera", image: "https://times.mw/wp-content/uploads/2023/07/lazarus-chakwera-2-860x1014.jpg", hint: "malawian man politician", totalBets: 75000 },
-  { id: 2, name: "Peter Mutharika", image: "https://upload.wikimedia.org/wikipedia/commons/1/16/Arthur_Peter_Mutharika_2014_%28cropped%29.jpg", hint: "malawian man suit", totalBets: 62000 },
+  { id: 2, name: "Peter Mutharika", image: "https://www.sadc.int/sites/default/files/2022-02/Peter-Mutharika.jpg", hint: "malawian man suit", totalBets: 62000 },
   { id: 3, name: "Saulos Chilima", image: "https://placehold.co/202x202.png", hint: "malawian man glasses", totalBets: 48000 },
   { id: 4, name: "Atupele Muluzi", image: "https://www.nyasatimes.com/wp-content/uploads/ATUPELE-MINISTER.jpg", hint: "malawian man leader", totalBets: 35000 },
 ]
