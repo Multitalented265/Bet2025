cfc8570dd59972d6d7b2c4afdbda34a289a786cc
multitalented box <multitalentedbox@gmail.com>
2025-07-24 11:46:42 +0000

    Thanks for sharing your code. If you're getting logged out immediately a

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -47,12 +47,16 @@ export const authOptions: NextAuthOptions = {
         async jwt({ token, user }) {
             if (user) {
                 token.id = user.id;
+                token.email = user.email;
+                token.name = user.name;
             }
             return token;
         },
         async session({ session, token }) {
             if (session.user) {
                 (session.user as NextAuthUser & { id: string }).id = token.id as string;
+                session.user.email = token.email;
+                session.user.name = token.name;
             }
             return session;
         }
